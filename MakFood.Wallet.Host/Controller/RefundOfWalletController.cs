51183fafaf46ff4e0518e21a5f2306594edbb115
saman salehi <samansalehi20051384@gmail.com>
2025-10-19 13:34:25 -0700

    اضافه کردن کنترلر

---
+++ MakFood.Wallet.Host/Controller/RefundOfWalletController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Commands.WithdrawWallet;
+using MakFood.Wallet.Application.CommandHandlers.RefundOfWallet;
+using MakFood.Wallet.Domain.Model.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RefundOfWalletController : ControllerBase
+{
+    private readonly RefundOfWalletCommandHandller _handler;
+
+    public RefundOfWalletController(IWalletRepository walletRepository)
+    {
+        _handler = new RefundOfWalletCommandHandller(walletRepository);
+    }
+
+    [HttpPost("RefundOfWallet")]
+    public async Task<IActionResult> RefundOfWallet([FromBody] RefundOfWalletCommand command)
+    {
+        var result = await _handler.HandleAsync(command);
+        if (!result.Success)
+            return BadRequest(result);
+
+        return Ok(result);
+    }
+}
