a6a626d91cce49a79f5f320c507e1c5a022d7d0c
saman salehi <samansalehi20051384@gmail.com>
2025-10-19 10:21:57 -0700

    اضافه کردن رپوزیتوری به تسک تراکنش ها

---
+++ MakFood.Wallet.Application/QueryHandlers/TranslationOfTransaction/TranslationOfTransactionQuery.cs
@@ -0,0 +1,17 @@
+﻿using MakFood.Wallet.Application.CommandHandlers.Base;
+using MakFood.Wallet.Application.CommandHandlers.Base.Extensions;
+using MediatR;
+
+namespace MakFood.Wallet.Application.QueryHandlers.Transaction
+{
+    public class TranslationOfTransactionQuery : QueryBase, IRequest<TranslationOfTransactionQueryResponse>
+    {
+        public Guid WalletId { get; set; }
+        public DateTime dateTime { get; set; }
+
+        public override void Validate()
+        {
+            new TranslationOfTransactionQueryValidator().Validate(this).ThrowIfNeeded();
+        }
+    }
+}
